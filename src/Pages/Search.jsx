bb84c92da674fcc4cf330785dcc2d61ef79eb5fe
Abdullatif-Ahmed <apdoahmed795@gmail.com>
2023-01-25 15:56:34 +0200

    adding margin to filter li

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -183,7 +183,7 @@ const Search = () => {
                     <div>
                       <ul className="d-flex flex-wrap align-items-center">
                         {filters.map((fl) => (
-                          <li key={fl.val} className="me-2">
+                          <li key={fl.val} className="me-2 mb-2">
                             <button
                               className="deleteFilter  rounded-pill d-flex justify-content-between align-items-center"
                               onClick={() =>
